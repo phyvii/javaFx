8cdf96f8894d3d76ae56ce0651eccb5b914c83e9
Krystian Gluszczak <s25586@pjwstk.edu.pl>
2026-04-23 13:59:21 +0200

    fullscreen stage

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 public class HelloController {
@@ -15,6 +16,21 @@ public class HelloController {
     private Button buttonScenes;
     @FXML
     private Button nextScene;
+    @FXML
+    private Button fullscreen;
+
+
+    @FXML
+    protected void setFullscreen() throws Exception{
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("fullscreen-view.fxml"));
+        Stage stage = (Stage) fullscreen.getScene().getWindow();
+        stage.setScene(new Scene(loader.load(),400,300));
+        stage.setTitle("fullscreen");
+
+        stage.setFullScreenExitHint("");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("Q"));
+        stage.setFullScreen(true);
+    }
 
     @FXML
     protected void onButtonClick() {
--- src/main/java/com/example/MainApp.java
+++ src/main/java/com/example/MainApp.java
@@ -3,9 +3,15 @@ package com.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApp extends Application {
 
@@ -14,8 +20,12 @@ public class MainApp extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(
                 MainApp.class.getResource("hello-view.fxml")
         );
-        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        Scene scene = new Scene(fxmlLoader.load());
+        Image icon = new Image(Objects.requireNonNull(MainApp.class.getResourceAsStream("pexels-neodc-16412582.jpg")));
         stage.setTitle("Moja aplikacja JavaFX");
+        stage.getIcons().add(icon);
+        stage.setWidth(800);
+        stage.setHeight(600);
         stage.setScene(scene);
         stage.show();
     }
